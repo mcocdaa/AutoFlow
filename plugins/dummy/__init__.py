387900b90cda6bcf78c5a39a8abe7229e317928b
mcocdaa <2021137961@qq.com>
2026-08-11 13:22:00 +0800

    refactor(plugins): migrate dummy plugin to Plugin base class

--- plugins/dummy/__init__.py
+++ plugins/dummy/__init__.py
@@ -1,7 +1,9 @@
 # @file /plugins/dummy/__init__.py
 # @brief Dummy 插件入口
 # @create 2026-03-27
+# @update 2026-08-10 迁移为 Plugin 基类新 ABI(register 为过渡期保留,Task 15 移除)
 
+from plugins.dummy.backend import PLUGIN
 from plugins.dummy.hooks import register
 
-__all__ = ["register"]
+__all__ = ["PLUGIN", "register"]
