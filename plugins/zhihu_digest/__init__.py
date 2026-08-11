df45e3ba31614ee167a53d6e460086a86fa89059
mcocdaa <2021137961@qq.com>
2026-08-11 13:32:57 +0800

    refactor(plugins): migrate zhihu_digest plugin to Plugin base class

--- plugins/zhihu_digest/__init__.py
+++ plugins/zhihu_digest/__init__.py
@@ -1,7 +1,9 @@
 # @file /plugins/zhihu_digest/__init__.py
 # @brief 知乎摘要插件入口
 # @create 2026-03-27
+# @update 2026-08-10 迁移为 Plugin 基类新 ABI(register 为过渡期保留,Task 15 移除)
 
+from plugins.zhihu_digest.backend import PLUGIN
 from plugins.zhihu_digest.hooks import register
 
-__all__ = ["register"]
+__all__ = ["PLUGIN", "register"]
